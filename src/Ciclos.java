ad2b89d698875597d2b85b723c7bf0015e8b8792
desarrollador-Lina <desarrolladorprincipalpollen@gmail.com>
2026-06-03 18:14:03 -0500

    Problema dia 4.

---
+++ src/Ciclos.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+class Ciclos{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int suma = a;
+
+            for(int j=0; j<n;j++){
+                int potencias = (int) Math.pow(2,j);
+                suma = suma +(potencias*b);
+                System.out.print(suma + " ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
