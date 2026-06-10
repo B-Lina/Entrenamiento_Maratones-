aa00078d78da57ccd39f2280ddb83fa1ec6369e6
desarrollador-Lina <desarrolladorprincipalpollen@gmail.com>
2026-06-09 21:12:23 -0500

    Dia 1-Iniciamos

---
+++ src/DiadelaSemana.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import java.time.LocalDate;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static String findDay(int month, int day, int year) {
+        LocalDate fecha = LocalDate.of(year,month,day);
+        return fecha.getDayOfWeek().name();
+    }
+}
+
+public class DiadelaSemana {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
--- src/Practicas.java
+++ src/Practicas.java
@@ -1,37 +1,37 @@
-//import java.util.Scanner;
+////import java.util.Scanner;
+////
+////public class Practicas {
+//// Formatind Output
+////    public static void main(String[] args) {
+////        Scanner sc=new Scanner(System.in);
+////        System.out.println("================================");
+////        for(int i=0;i<3;i++){
+////            String s1=sc.next();
+////            int x=sc.nextInt();
+////            System.out.printf("%-15s%03d%n", s1, x);
+////        }
+////        System.out.println("================================");
+////
+////    }
+////}
 //
+//// Buffered Int
+//import java.io.*;
+//import java.math.*;
+//import java.security.*;
+//import java.text.*;
+//import java.util.*;
+//import java.util.concurrent.*;
+//import java.util.regex.*;
+//// Close el buffered
 //public class Practicas {
-// Formatind Output
-//    public static void main(String[] args) {
-//        Scanner sc=new Scanner(System.in);
-//        System.out.println("================================");
-//        for(int i=0;i<3;i++){
-//            String s1=sc.next();
-//            int x=sc.nextInt();
-//            System.out.printf("%-15s%03d%n", s1, x);
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        int N = Integer.parseInt(bufferedReader.readLine().trim());
+//        bufferedReader.close();
+//        for (int i = 1; i<11;i++){
+//            System.out.println(N + " x " + i + " = " + (N * i));
 //        }
-//        System.out.println("================================");
-//
 //    }
 //}
-
-// Buffered Int
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
-// Close el buffered
-public class Practicas {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine().trim());
-        bufferedReader.close();
-        for (int i = 1; i<11;i++){
-            System.out.println(N + " x " + i + " = " + (N * i));
-        }
-    }
-}
 
